181439f37ace9823d6033198bd55e49728f79438
Cerberus2022 <97562028+Cerberus2022@users.noreply.github.com>
2022-03-06 10:02:51 -0800

    Update Morning

--- src/micropython/closedLoopControl.py
+++ src/micropython/closedLoopControl.py
@@ -32,7 +32,7 @@ class ClosedLoopController:
         # Setting some parameters
         self.final_point = 0
         self.kp = .5
-        self.gain = 0  # the gain will be updated in a function
+        self.gain = 0  												# the gain will be updated in a function
         self.encoder = encoder
         self.motor = motor
 
@@ -49,24 +49,24 @@ class ClosedLoopController:
                     actuation value; which is dependent of the difference
                     encoder positions as well as the value of kp
         '''
-        self.encoder.set_position(0)  # zero out the encoder value
+        self.encoder.set_position(0)  								# zero out the encoder value
         # NOTE This clc will run endlessly for the robotic arm mission
         while True:
-            self.encoder.update()  # update the encoder value
-            error = self.final_point - self.encoder.current_pos  # get the error
-            voltage = 3.3  # Voltage used for the micro-controller
-            actuation = (error * self.kp) / voltage  # get the actuation
-            if actuation >= 90:  # dont let it go too fast
+            self.encoder.update()  									# update the encoder value
+            error = self.final_point - self.encoder.current_pos     # get the error
+            voltage = 3.3  											# Voltage used for the micro-controller
+            actuation = (error * self.kp) / voltage  	 			# get the actuation
+            if actuation >= 90: 						 			# It does not let it go too fast
                 self.motor.set_duty_cycle(90)
-            elif 20 >= actuation > 5:  # dont let it stall
+            elif 20 >= actuation > 5: 							    # It does not let it stall
                 self.motor.set_duty_cycle(20)
-            elif -20 <= actuation < 5:  # dont let it stall
+            elif -20 <= actuation < 5: 							    # It does not let it stall
                 self.motor.set_duty_cycle(-20)
-            elif actuation <= 90:  # dont let it go too fast
+            elif actuation <= 90:  									# It does not let it go too fast
                 self.motor.set_duty_cycle(-90)
-            elif -5 <= actuation <= 5:  # we are there
-                self.motor.set_duty_cycle(0)  # stop the motor until we get another position
-            else:  # keep going
+            elif -5 <= actuation <= 5:  							# we are there
+                self.motor.set_duty_cycle(0)  						# stop the motor until we get another position
+            else:  													# keep going
                 self.motor.set_duty_cycle(actuation)
-            #print(f"final Point {self.final_point}, current point {self.encoder.current_pos}, accucation {actuation}")
-            yield 0  # let the next task do its job
+            #print(f"final Point {self.final_point}, current point {self.encoder.current_pos}, actuation {actuation}")
+            yield 0  												# let the next task do its job
--- src/micropython/servo.py
+++ src/micropython/servo.py
@@ -7,7 +7,7 @@ import math
 class Servo:
     def __init__(self, pin, timer_number, channel_number):
         self.pin = pin  # save the pin number
-        # setup the timer with the frequency from the datasheet
+        # setup the timer with the frequency from the data-sheet
         self.timer = pyb.Timer(timer_number, freq=50)
         self.channel = self.timer.channel(channel_number, pyb.Timer.PWM, pin=self.pin)
 
@@ -16,6 +16,6 @@ class Servo:
         self.SetAngle(degree)
 
     def SetAngle(self, degree):
-        pulse = degree * (2/3)  # update the conversion factor
-        self.channel.pulse_width(pulse)  # set the pulse width
+        pulse = degree * (2/3)  		     # update the conversion factor
+        self.channel.pulse_width(pulse) 	 # set the pulse width
         pass
